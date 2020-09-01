e88b1b004463707f85bb922ca10843ae66183d8a
jean philippe <bongojeanphilippe@gmail.com>
2020-09-01 18:15:15 +0200

    allow header

--- index.js
+++ index.js
@@ -26,7 +26,8 @@
      // Website you wish to allow to connect
      res.header({
          'content-type': 'application/json',
-         'Access-Control-Allow-Origin': "http://127.0.0.1:3000"
+         'Access-Control-Allow-Origin': "http://127.0.0.1:3000",
+         'Access-Control-Allow-Headers': "authorization",
      });
 
 
