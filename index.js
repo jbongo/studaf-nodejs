eab57ef779403c5445cd87a61515e877146b8cd5
Jean Philippe <bongojeanphilippe@gmail.com>
2020-10-04 03:35:56 +0200

    add origin

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
 
  app = express();
  const PORT = process.env.PORT || 4001;
- const ORIGIN_URL = process.env.ORIGIN_URL || "http://127.0.0.1:3000";
+ const ORIGIN_URL = "http://127.0.0.1:3000";
+//  const ORIGIN_URL = process.env.ORIGIN_URL || "http://127.0.0.1:3000";
 
  // connection a mongodb
 
